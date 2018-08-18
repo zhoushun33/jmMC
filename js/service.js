dfa31acc1cd790b89d2019e1ac72beaa228e013e
zhoushun33 <1071207477@qq.com>
2018-08-18 09:15:36 +0800

    2018年8月18日09:15:30

---
+++ js/service.js
@@ -0,0 +1,8 @@
+/*
+ *服务器参数
+ */
+url='http://192.168.0.183:8081/jmmcPC/jmmcbg/';//602
+//url='http://202.105.179.167:8080/jmConstr/jmadmin/';//服务器
+//url='http://182.61.39.167/jmConstr/jmadmin/';//服务器
+//var servername='root';
+//var serverpassword='123456';
