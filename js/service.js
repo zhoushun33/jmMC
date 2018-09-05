b57537eddff9088b9decdd70b6b4d8e0a2ed7f32
zhoushun33 <1071207477@qq.com>
2018-09-05 16:10:29 +0800

    2018年9月5日16:10:25

--- js/service.js
+++ js/service.js
@@ -1,8 +1,8 @@
 /*
  *服务器参数
  */
-url='http://192.168.0.184:8081/jmmcPC/jmmcbg/';//602
-//url='http://202.105.179.167:8080/jmConstr/jmadmin/';//服务器
+//url='http://192.168.0.184:8081/jmmcPC/jmmcbg/';//602
+url='http://47.107.73.98/jmmcPC/jmmcbg/';//服务器
 //url='http://182.61.39.167/jmConstr/jmadmin/';//服务器
 //var servername='root';
 //var serverpassword='123456';
